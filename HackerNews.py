6b39b0f476c59734e827c2a6a86d58658bbd797a
rohiiit <44130215+rohiiit@users.noreply.github.com>
2019-07-31 22:26:44 +0530

    HackerNews Data Cleaned

--- HackerNews.py
+++ HackerNews.py
@@ -6,13 +6,17 @@ url = "https://hacker-news.firebaseio.com/v0/topstories.json"
 payload = "{}"
 response = requests.request("GET", url, data=payload)
 a = response.text
-print(type(a))
+
 
 #Converting string list into list
 a = ast.literal_eval(a)
 
-for i in range(len(a)):
+for i in range(2):
 	url = "https://hacker-news.firebaseio.com/v0/item/"+str(a[i])+ ".json"
 	payload = "{}"
 	response = requests.request("GET", url, data=payload)
-	print(response.text)
+	dict_response = response.json()
+	print("Title: " + str(dict_response['title']))
+	print("URL: " + str(dict_response['url']))
+	#print(dict_response)
+	
